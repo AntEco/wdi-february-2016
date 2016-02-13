30cc9c10305e57863c7247f68cb57b4ee3b9ad20
Orlando Caraballo <orlando@nycda.com>
2016-02-13 11:04:08 -0500

    Added some commenting

--- 02-intro-to-javascript/day-5/css3-demo.js
+++ 02-intro-to-javascript/day-5/css3-demo.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $("a").click(function(event){
+    // every time you click the anchor it will either
+    //  add the following classes or remove them
+    // the styling and transitioning will be handled by css
     $(event.currentTarget).toggleClass("transition")
     $("h1").toggleClass("bear")
     $("strong").toggleClass("hidden")
